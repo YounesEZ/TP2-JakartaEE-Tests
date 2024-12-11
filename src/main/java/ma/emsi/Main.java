59d1dfb758d4e15a81d6c60552a960b6e98e301a
YounesEZ <ezbidayounes@gmail.com>
2024-12-11 12:25:33 +0100

    Test1

---
+++ src/main/java/ma/emsi/Main.java
@@ -0,0 +1,36 @@
+package ma.emsi;
+
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.request.ResponseFormat;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiEmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
+import dev.langchain4j.store.embedding.CosineSimilarity;
+
+import java.time.Duration;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        ChatLanguageModel model = GoogleAiGeminiChatModel.builder()
+                .temperature(0.7)
+                .modelName("gemini-1.5-flash")
+                .apiKey(System.getenv("GEMINI_KEY"))
+                .build();
+     //Test 1
+        String message = "Explain GenAI";
+
+        String stringResponce = model.generate(message);
+
+        System.out.println(stringResponce);
+
+
+    }
+}
